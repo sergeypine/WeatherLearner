cc6bf360ab7fcc46be990807c639f34efe529f7d
Sergey Numerov <sergey.vn@hotmail.com>
2021-01-29 15:32:33 -0800

    added more cloud/precepitation data to wunderground_clearner output

--- data/analysys/wunderground_cleaner.py
+++ data/analysys/wunderground_cleaner.py
@@ -29,8 +29,8 @@ def cleanFile(filePath):
 	#Transform wind direction into 2 components: Northerly and Easterly
 	raw_df['WindNortherly'], raw_df['WindEasterly'] = zip(*raw_df['Wind'].apply(getWindDirectionNEComponents))
 
-	#Transform condition into 4 flags: is_clear, is_precip, is_heavy_precip, is_tstorm
-	raw_df['is_clear'], raw_df['is_precip'], raw_df['is_heavy_precip'], raw_df['is_tstorm'] = zip(
+	#Transform condition into 7 flags: is_clear, is_partly_cloudy, is_fog, is_precip, is_light_precip, is_heavy_precip, is_tstorm
+	raw_df['is_clear'], raw_df['is_partly_cloudy'], raw_df['is_fog'], raw_df['is_precip'], raw_df['is_heavy_precip'], raw_df ['is_light_precip'], raw_df['is_tstorm'] = zip(
 		*raw_df['Condition'].apply(convertConditionToFlags))
 
 	#Round all timestamps to 1h
@@ -52,6 +52,9 @@ def cleanFile(filePath):
 	cleaned_df['hour_of_day_cos'] = np.cos(2 * np.pi * cleaned_df['Time'].dt.hour / 24)
 
 	cleaned_df = cleaned_df.sort_values(by ='Time')
+
+	#Cutoff all data before 03/15/2014 - it is missing precipitation data
+	cleaned_df = cleaned_df[cleaned_df['Time'] > pd.to_datetime('2014-03-14')]
 
 	return cleaned_df
 
@@ -76,32 +79,45 @@ wind_NE_component_dict = {
 	'E': [0, 1], 
 	'NE': [0.5, 0.5]
 }
+#=================================================
 def getWindDirectionNEComponents(windDirection):
 	return wind_NE_component_dict[windDirection][0],wind_NE_component_dict[windDirection][1]
 
 #=================================================
 def convertConditionToFlags(condition):
-	is_clear = False
-	is_precip = False
-	is_heavy_precip = False
-	is_tstorm = False
+	is_clear = 0
+	is_partly_cloudy = 0
+	is_fog = 0
+	is_precip = 0
+	is_heavy_precip = 0
+	is_light_precip = 0
+	is_tstorm = 0
 
 	lcondition = condition.lower()
 
 
 	if (('fair' in lcondition) or ('smoke' in lcondition) or ('haze' in lcondition)):
-		is_clear = True
+		is_clear = 1
 
-	if (('rain' in lcondition) or ('drizzle' in lcondition) or ('snow' in lcondition) or ('t-storm' in lcondition) or ('wintry mix' in lcondition)):
-		is_precip = True
+	if (('partly' in lcondition) or ('mostly' in lcondition)):
+		is_partly_cloudy = 1
+
+	if 'fog' in lcondition:
+		is_fog = 1		
+
+	if (('rain' in lcondition) or ('drizzle' in lcondition) or ('snow' in lcondition) or ('t-storm' in lcondition) or ('wintry mix' in lcondition) or ('hail' in lcondition) or ('precipitation' in lcondition)):
+		is_precip = 1
 
 	if 'heavy' in lcondition:
-		is_heavy_precip = True	
+		is_heavy_precip = 1	
+
+	if 'light' in lcondition:
+		is_light_precip = 1
 
 	if (('t-storm' in lcondition) or ('thunder' in lcondition)):
-		is_tstorm = True
+		is_tstorm = 1
 
-	return is_clear, is_precip, is_heavy_precip, is_tstorm
+	return is_clear, is_partly_cloudy, is_fog, is_precip, is_heavy_precip, is_light_precip, is_tstorm
 
 #=================================================
 def getCleanFileName(filePath):
@@ -110,11 +126,13 @@ def getCleanFileName(filePath):
 #=================================================
 
 files_to_clean = ['../Chicago_2011-2020.csv', 
-	'../Madison_2011-2020.csv', '../GreenBay_2011-2020.csv', '../GrandRapids_2011-2020.csv', '../Detroit_2011-2020.csv',
-	'../Cincinatti_2011-2020.csv', '../Indianapolis_2011-2020.csv', '../StLouis_2011-2020.csv', '../Des_Moines_2011-2020.csv']
+	'../Toledo_2011-2020.csv', '../Peoria_2011-2020.csv', '../CedarRapids_2011-2020.csv', '../Saginaw_2011-2020.csv', '../FortWayne_2011-2020.csv', '../Milwaukee_2011-2020.csv',
+	'../Indianapolis_2011-2020.csv', '../Madison_2011-2020.csv', '../GreenBay_2011-2020.csv', '../GrandRapids_2011-2020.csv',
+	'../Cleveland_2011-2020.csv', '../Columbus_2011-2020.csv', '../Cincinatti_2011-2020.csv', '../StLouis_2011-2020.csv', '../Des_Moines_2011-2020.csv', '../Minneapolis_2011-2020.csv', 
+	'../Sault_Ste_Marie_2011-2020.csv', '../Pittsburgh_2011-2020.csv', '../Toronto_2011-2020.csv', '../KansasCity_2011-2020.csv', '../Duluth_2011-2020.csv']
 cleaned_count = 0
 for file_to_clean in files_to_clean:
 	cleaned_df = cleanFile(file_to_clean)
 	cleaned_df.to_csv(getCleanFileName(file_to_clean), index = False)
 	cleaned_count = cleaned_count + 1
-	print("Cleaned & Saved {} files out of {}".format(cleaned_count, len(files_to_clean)))
+	print("Cleaned & Saved {} files out of {}, line count = {}".format(cleaned_count, len(files_to_clean), len(cleaned_df)))
